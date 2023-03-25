d2cddce982cdc398b29dae4b7adca9a3d5691697
iselo <ok+github@raccoons.co>
2023-03-25 12:49:11 +0200

    Init

---
+++ src/main/index.ts
@@ -0,0 +1,11 @@
+import Annotation from "./Annotation";
+import Any from "./Any";
+import Class from "./Class";
+import Method from "./Method";
+
+export {
+    Annotation,
+    Any,
+    Class,
+    Method
+}
---
+++ src/test/TypesTest.ts
@@ -0,0 +1,39 @@
+import {Immutable, Test} from "@raccoons-co/cleanway";
+import {assert} from "chai";
+import {Annotation, Any, Class, Method} from "../main/index";
+
+@Immutable
+export default class TypesTest implements Annotation {
+
+    decorator(): Method {
+        return this.method;
+    }
+
+    @Test
+    private canUseAnyType() {
+        assert.isString(new TypesTest().any("Nice"));
+        assert.isFunction(new TypesTest().any( TypesTest ));
+    }
+
+    @Test
+    private canUseClassType() {
+        assert.isFunction(new TypesTest().class( TypesTest ));
+    }
+
+    @Test
+    private canUseMethodType() {
+        assert.isFunction(new TypesTest().method( new TypesTest().any ));
+    }
+
+    public any(value: Any): Any {
+        return value;
+    }
+
+    public class(value: Class): Class {
+        return value;
+    }
+
+    public method(value: Method): Method {
+        return value;
+    }
+}
