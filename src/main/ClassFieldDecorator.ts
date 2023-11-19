91e806d95de6e393cfefe36e62e5771508d53f31
iselo <ok+github@raccoons.co>
2023-11-20 00:54:52 +0200

    Redesign `EnvironmentVariable`

---
+++ src/main/ClassFieldDecorator.ts
@@ -0,0 +1,20 @@
+/*
+ *  Copyright 2023, Raccoons. Developing simple way to change.
+ *
+ *  @license MIT
+ */
+
+import {Any, Method} from "@raccoons-co/genera";
+import {Strict} from "@raccoons-co/ethics";
+import Annotation from "./Annotation";
+
+export default class ClassFieldDecorator implements Annotation {
+
+    decorator(initializer: Method): Method {
+        return function newFieldInitializer(value: Any, context: ClassFieldDecoratorContext) {
+            Strict.notNull(context);
+            Strict.checkArgument(context.kind === "field");
+            return initializer;
+        };
+    }
+}
--- src/main/EnvironmentVariable.ts
+++ src/main/EnvironmentVariable.ts
@@ -5,29 +5,34 @@
  */
 
 import {Strict} from "@raccoons-co/ethics";
-import {Annotation, Any, Method} from "@raccoons-co/genera";
+import {Method} from "@raccoons-co/genera";
+import ClassFieldDecorator from "./ClassFieldDecorator";
 
 /**
  * The user environment variable.
+ *
+ * @param key - the name of the environment variable
+ * @returns object - the decorator factory instance
  */
-class EnvironmentVariable implements Annotation {
+export default function EnvironmentVariable(key: string) {
+    return new class DecoratorFactory {
 
-    /**
-     * Returns the class field decorator that replaces initial field value
-     * with value of user environment variable.
-     *
-     * @param key - the name of the environment variable
-     * @returns function - the class field decorator
-     */
-    decorator(key: string): Method {
-        Strict.notNull(key);
-        return function newFieldValue(value: Any, context: ClassFieldDecoratorContext) {
-            Strict.notNull(context);
-            Strict.checkArgument(context.kind === "field");
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            return (initialValue: string) => Strict.notNull(process.env[key], `${key} is undefined`);
-        };
-    }
+        /** Returns decorator that replaces initial field value with value of user environment variable if defined. */
+        public orInitial(): Method {
+            return new ClassFieldDecorator().decorator(
+                (initialValue: string) => process.env[key] ?? initialValue
+            );
+        }
+
+        /**
+         * Returns decorator that replaces initial field value with value of defined user environment variable
+         * otherwise throws `NullPointerException`.
+         */
+        public orElseThrow(): Method {
+            return new ClassFieldDecorator().decorator(
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                (initialValue: string) => Strict.notNull(process.env[key], `${key} is undefined`)
+            );
+        }
+    };
 }
-
-export default new EnvironmentVariable().decorator;
